f2e23e92c9b74515798d33cb27fe903dc104db74
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-22 06:40:51 +0000

    use Dagger Secret for GITLAB_ACCESS_TOKEN

--- src/dagger/lib.ts
+++ src/dagger/lib.ts
@@ -1,4 +1,9 @@
-import Client, { Directory, DirectoryID } from "../../deps.ts";
+import Client, {
+  Directory,
+  DirectoryID,
+  Secret,
+  SecretID,
+} from "../../deps.ts";
 
 export const getDirectory = (
   client: Client,
@@ -10,4 +15,23 @@ export const getDirectory = (
     });
   }
   return src instanceof Directory ? src : client.host().directory(src);
+};
+
+export const getGitlabToken = (client: Client, token?: string | Secret) => {
+  if (Deno.env.get("GITLAB_ACCESS_TOKEN")) {
+    return client.setSecret(
+      "GITLAB_ACCESS_TOKEN",
+      Deno.env.get("GITLAB_ACCESS_TOKEN")!
+    );
+  }
+  if (token && typeof token === "string") {
+    if (token.startsWith("core.Secret")) {
+      return client.loadSecretFromID(token as SecretID);
+    }
+    return client.setSecret("GITLAB_ACCESS_TOKEN", token);
+  }
+  if (token && token instanceof Secret) {
+    return token;
+  }
+  return undefined;
 };
